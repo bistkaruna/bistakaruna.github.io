6dca0ea89845023e7319acd2414b79bf83efcb40
bistkaruna <166574944+bistkaruna@users.noreply.github.com>
2024-06-26 08:42:09 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+//variables
+// var x=90;
+// console.log(x);
+
+// var x=500;
+// console.log(x);
+
+// let y=90;
+// console.log(y);
+//  y=900;
+// console.log(y);
+
+// const result=9+9
+// console.log(result)
+
+// const result=6-4;
+// console.log(result);
+
+// Data types
+// number
+// let x=90;
+// console.log(x);
+// console.log(typeof x);
+
+// String
+// const result="Karuna Bist"
+// console.log(result);
+// console.log?(typeof result);
+
+// undefined
+// let a;
+// console.log(a);
+// console.log(typeof a);
+
+// boolean
+// const isTrue=true;
+// console.log(isTrue);
+// console.log(typeof isTrue)
+
+//  null(object)
+// const y=null;
+// console.log(y);
+//  console.log(typeof y);
+
+// Float
+// const result=5.78;
+// console.log(result);
+// console.log(typeof result);
+
